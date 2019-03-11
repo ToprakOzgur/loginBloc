703e2a0e646381bc6253e6d830d542bb4c366a58
ToprakOzgur <zgrtprk@gmail.com>
2019-03-11 13:29:22 +0300

    completed with RxDart

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -56,11 +56,7 @@ class LoginScreen extends StatelessWidget {
         return RaisedButton(
           child: Text('Login'),
           color: Colors.blue,
-          onPressed: snapshot.hasError
-              ? null
-              : () {
-                  print('Hi There');
-                },
+          onPressed: snapshot.hasData ? bloc.submit : null,
         );
       },
     );
